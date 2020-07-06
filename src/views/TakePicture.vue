e3423f7536f1a76f8402150e81d62b080fbb5fe2
jlpereira <pepelui@outlook.com>
2020-07-06 13:33:36 -0300

    Agregado componente

---
+++ src/views/TakePicture.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+
+import CamaraComponent from '@/components/Camera.vue'
+
+export default {
+  components: {
+    CameraComponent
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
