9da2896046107e1f4d31a032348e76a3b45a6c32
José Luis Pereira <pepelui@outlook.com>
2020-10-27 17:49:23 -0300

    Cambiada ruta de la API

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const apiRequest = {
   // host: '127.0.0.1.2', // Localhost
   // host: '10.0.2.2', // Android
   host: '192.168.0.210',
-  path: '/api',
-  port: '8080',
+  path: '',
+  port: '1337',
   protocol: 'http'
 }
 
